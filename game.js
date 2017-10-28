64b04c994417c97f7565be6c3ef9403ad5105805
xFlowDev <f.loed@web.de>
2017-10-28 02:26:28 +0200

    Movement of Creep working

--- game.js
+++ game.js
@@ -1,42 +1,55 @@
-// // var game = game || {};
-// game = {
-//     //Class Variables
-
-//     isRunning: true,
-//     // To Stop the interval
-//     intervalId: 0,
-//     i: 0,
-
-
-//     // Functions 
-//     start: function () {
-//         canvas = document.getElementById("game");
-//         canvasContext = canvas.getContext("2d");
-//         if(this.isRunning){
-//             this.intervallId = setInterval(this.update, 1000 / 60);
-//             console.log(this.intervalId);
-//         }
-//     },
-//     update: function () {
-//         console.log("");
-//     },
-//     stop: function() {
-//         clearInterval(this.intervalId);
-//         this.isRunning = false;
-//     }
-// }
-// window.onload = game.start();
-
 //Phaser Test
-
 window.onload = function () {
-    var game = new Phaser.Game(800, 600, Phaser.AUTO, '', {preload: preload, create: create});
+    var game = new Phaser.Game("99%", "99%",
+        Phaser.AUTO, '',
+        {
+            preload: preload,
+            create: create,
+            update: update
+        });
 
-    function preload(){
-        // Sachen die vorgeladen werden sollen
+    function preload() {
+        // Preload Sprites and other Assets
+        
+        // [TODO] Start Positionen noch anpassen
+        game.load.image('ground', 'assets/ground.png');
+        game.load.spritesheet('creep', 'assets/creep.png', 350, 350);
+        
     }
 
-    function create(){
+
+    // I have to declare these objects so I can use them in the update function
+    var ground;
+    var creep;
+    var cursors;
+    // Basically an initialization function
+    function create() {
+        game.stage.backgroundColor = 0xFFFFFF;
+
+        ground = game.add.sprite(0, 400, 'ground');
+
+        creep = game.add.sprite(0, 0, 'creep');
+        creep.animations.add('right', [0, 1, 2, 3, 4, 5], 10, true);
+
+        cursors = game.input.keyboard.createCursorKeys();
+    }
+
+
+    var cVel = 10;
+    function update() {
+        // Scale the game to always fit the Screen
+        var documentElement = document.documentElement;
+        var width = documentElement.clientWidth - 20;
+        var height = documentElement.clientHeight - 20;
+        game.scale.setGameSize(width, height);
+
+        // Simple Creep Movement from left to right
+        // If the creep goes further then the screen is wide it will reset
+
+        creep.x += cVel;
+        if(creep.x > width - creep.width){
+            creep.x = 0;
+        }
     }
 
 }
