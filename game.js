159e8166835ff85244fcdeb7f4f5694b3ab5d623
xFlowDev <f.loed@web.de>
2017-10-28 11:25:54 +0200

    Structured code and added mouse pointer position text

--- game.js
+++ game.js
@@ -1,55 +1,114 @@
 //Phaser Test
 window.onload = function () {
-    var game = new Phaser.Game("99%", "99%",
+    var documentElement = document.documentElement;
+    var width = documentElement.clientWidth - 20;
+    var height = documentElement.clientHeight - 20;
+    var scaleFactor = 1;
+
+
+    var game = new Phaser.Game(width, height,
         Phaser.AUTO, '',
         {
             preload: preload,
             create: create,
-            update: update
+            update: update,
+            render: render
         });
 
     function preload() {
         // Preload Sprites and other Assets
-        
-        // [TODO] Start Positionen noch anpassen
         game.load.image('ground', 'assets/ground.png');
-        game.load.spritesheet('creep', 'assets/creep.png', 350, 350);
-        
+        game.load.spritesheet('creep', 'assets/creep.png', 350, 350, 6);
+
+        // game.load.spritesheet('player', 'assets/player.png', x, y);
+        // game.load.image('circuit', 'assets/circuit_board.png');
+        // game.load.image('background', 'assets/background.png');
     }
 
 
     // I have to declare these objects so I can use them in the update function
+    var stageGroup;
     var ground;
+
+    var entityGroup;
     var creep;
+    var cWalk;
+    var player;
+
+    var guiGroup;
+    var debugStats;
+
     var cursors;
+    
     // Basically an initialization function
     function create() {
         game.stage.backgroundColor = 0xFFFFFF;
 
-        ground = game.add.sprite(0, 400, 'ground');
-
-        creep = game.add.sprite(0, 0, 'creep');
-        creep.animations.add('right', [0, 1, 2, 3, 4, 5], 10, true);
+        // This sets the display order of the Groups
+        stageGroupSetup();
+        entityGroupSetup();
+        guiGroupSetup();
 
         cursors = game.input.keyboard.createCursorKeys();
     }
 
-
     var cVel = 10;
+
+    var mouseX, mouseY;
+    var mousePositionText;
     function update() {
         // Scale the game to always fit the Screen
-        var documentElement = document.documentElement;
-        var width = documentElement.clientWidth - 20;
-        var height = documentElement.clientHeight - 20;
+        width = documentElement.clientWidth - 20;
+        height = documentElement.clientHeight - 20;
         game.scale.setGameSize(width, height);
 
         // Simple Creep Movement from left to right
         // If the creep goes further then the screen is wide it will reset
+        // creep.x += cVel;
+        // if (creep.x > width - creep.width) {
+        //     creep.x = 0;
+        // }
 
-        creep.x += cVel;
-        if(creep.x > width - creep.width){
-            creep.x = 0;
-        }
+        // Mouse Cursor Stats
+        mouseX = game.input.mousePointer.x;
+        mouseY = game.input.mousePointer.y;
+        mousePositionText = mouseX + "x" + mouseY;
+        debugStats.text = mousePositionText;
     }
 
+    function render() {
+        // game.debug.inputInfo(0, 500);
+    }
+
+    function guiGroupSetup() {
+        guiGroup = game.add.group();
+
+        var debugText = "";
+        var debugTextStyle = { font: "14pt Consolas", fill: "#00FF00", align: "left" };
+        debugStats = game.add.text(50, 50, debugText, debugTextStyle);
+        debugStats.anchor.set(0.5);
+        guiGroup.add(debugStats);
+
+    }
+
+    function entityGroupSetup(){
+        entityGroup = game.add.group();
+
+        creep = game.add.sprite(0, 0, 'creep');
+        creep.animations.add('walk');
+        creep.animations.play('walk', 10, true);
+        entityGroup.add(creep);
+    }
+
+    function stageGroupSetup(){
+        stageGroup = game.add.group();
+
+        ground = game.add.sprite(0, 400, 'ground');
+        stageGroup.add(ground);
+    }
+
+    function configGroupSetup(){
+        configGroup = game.add.group();
+        
+    }
 }
